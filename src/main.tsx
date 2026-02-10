c542f5631430d5d527787ad74470cd088e17c85a
isteppu <stefanieasaytono@gmail.com>
2026-02-10 17:17:24 +0800

    created structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router"
+import { RouterProvider } from "react-router/dom"
+import Home from './pages/Home'
 import './index.css'
-import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 )
